d653e2601e0e3360635faac6919f60124094ea75
Bilal Ibrahimov <ibrahimovbilal9@gmail.com>
2025-04-14 10:13:34 +0400

    repository interface is sent

---
+++ src/main/java/com/example/car_registration/repository/CarRepository.java
@@ -0,0 +1,14 @@
+package com.example.car_registration.repository;
+
+import com.example.car_registration.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+
+    Optional<Car> findByPlateNumber(String plateNumber);
+}
